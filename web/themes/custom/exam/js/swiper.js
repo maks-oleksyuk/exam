7308c2def49a0cbad4a7a4eed54289e9757fcebc
Maks <maksym.oleksyuk@gmail.com>
2021-12-13 13:14:38 +0200

    need_fix

---
+++ web/themes/custom/exam/js/swiper.js
@@ -0,0 +1,11 @@
+let plans = new Swiper(".swiper-services", {
+  grabCursor: true,
+  // spaceBetween: 27,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  breakpoints: {
+    769: {
+      centeredSlides: false,
+    },
+  },
+});
